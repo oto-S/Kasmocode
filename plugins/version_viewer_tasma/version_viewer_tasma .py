de74395f1d76066385335cf8caa49f14e2286c75
John Breno py <johnbrenosf@gmail.com>
2026-02-13 09:21:54 -0300

    Correct version data reading method
    Fix reading version data from file to ensure it captures the entire content.

--- plugins/version_viewer_tasma/version_viewer_tasma .py
+++ plugins/version_viewer_tasma/version_viewer_tasma .py
@@ -1,6 +1,6 @@
 def register(context):
     with open("../../docs/vssion.txt", "r", encoding="utf-8") as f:
-        context.version_data = f.read().strip()
+        context.version_data = f.read().read()
 
 
 #mais tarde irei faze o reto
